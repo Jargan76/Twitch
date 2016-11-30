bbf4f4389c645f5868188c7e510ff89a1d0ca241
Eric Hebel <Jargan76Studio@gmail.com>
2016-11-30 10:18:30 -0500

    Clean-up
    Just some clean up to the last commit

--- app/components/Cover.js
+++ app/components/Cover.js
@@ -8,15 +8,14 @@ export default class Cover extends React.Component {
                 <div>
                     <div className="inner cover">
                         <h1 className="cover-heading">Thank you.</h1>
-                        <p className="lead">Thanks for checking out my little project. This is a collection of my favorite twitch channels. I put this together as a project on 
-            <a href="http://freecodecamp.com/" target="_blank">
-                <span className="text-info">FreeCodeCamp</span>
-            </a><FontAwesome name='link' />
+                        <p className="lead">Thanks for checking out my little project. This is a collection of my favorite twitch channels. 
+                            I put this together as a project on <a href="http://freecodecamp.com/" target="_blank">
+                                <span className="text-info">FreeCodeCamp</span> </a><FontAwesome name='link' />
                         </p>
                         <h4>Because this is on GitHub</h4>
-                        <p>The XML request is denied for security reasons.  
-If you would like to fork and run on Node.js locally, it will work like a champ. I know there are workarounds that I could use. Most are pretty hacky and I'm ready to move on.
-Now click below to check it out.</p>
+                        <p>The XML request is denied for security reasons. If you would like to fork and run on Node.js locally, it will work like a champ. 
+                           I know there are workarounds that I could use. Most are pretty hacky and I'm ready to move on. Now click below to check it out.
+                        </p>
                         <p className="lead">
                             <Link to="/all" className="btn btn-lg btn-default">(<FontAwesome name='eye' /><FontAwesome name='eye' />)     =<FontAwesome name='hand-o-right' className="hand"/></Link>
                         </p>
