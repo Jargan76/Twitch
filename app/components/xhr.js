340e0fc84c4ff8a2b8c74eb5ebcecfcee2b48b19
Jargan76 <Jargan76Studio@gmail.com>
2017-01-09 11:36:39 -0500

    Update xhr.js
    ???

--- app/components/xhr.js
+++ app/components/xhr.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 let getChannel = function(name) {
-  return axios.get('https://wind-bow.gomix.me/twitch-api/streams/' + name)
+  return axios.get('https://wind-bow.gomix.me/twitch-api/streams/' + name,'jsonp')
 }
 export default getChannel
