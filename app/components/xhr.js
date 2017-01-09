fc49a4e3d8f81ef430a2f8a715c8a7fee136e047
Jargan76 <Jargan76Studio@gmail.com>
2017-01-09 09:59:37 -0500

    Update xhr.js
    update to api address.

--- app/components/xhr.js
+++ app/components/xhr.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 let getChannel = function(name) {
-  return axios.get('https://wind-bow.hyperdev.space/twitch-api/streams/' + name)
+  return axios.get('https://wind-bow.gomix.me/twitch-api/streams/' + name)
 }
 export default getChannel
